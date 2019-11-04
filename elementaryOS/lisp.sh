035007993ab93d8ce85864a4c4cd4f7b8d1f9372
noizhardware <noisefuel@gmail.com>
2019-11-04 20:42:56 +0100

    Update lisp.sh

--- elementaryOS/lisp.sh
+++ elementaryOS/lisp.sh
@@ -11,3 +11,31 @@ sudo apt install emacs25-nox
 sudo apt install e3
 sudo apt install emacs25-lucid
 sudo apt install jove
+
+sudo apt update
+sudo apt-get update
+
+
+# ---------------
+sudo -i
+sudo apt-get install sbcl
+sudo curl -o /tmp/ql.lisp http://beta.quicklisp.org/quicklisp.lisp
+sudo curl -o /tmp/ql.lisp http://beta.quicklisp.org/quicklisp.lisp
+sudo sbcl --no-sysinit --no-userinit --load /tmp/ql.lisp
+# then evaluate (quicklisp-quickstart:install) as asked
+# then also evaluate:
+# (quicklisp-quickstart:install :path "~/.quicklisp")
+# (ql:add-to-init-file)
+# (quit)
+sudo sbcl --eval '(ql:quickload :quicklisp-slime-helper)' --quit
+sudo nano .emacs
+# paste this into the file:
+# (load (expand-file-name "~/.quicklisp/slime-helper.el"))
+# (setq inferior-lisp-program "sbcl")
+# save and exit: Ctrl+O, Enter, Ctrl+X
+sudo emacs
+# ok, now you press Alt+X, and then type "slime", Enter
+# congrats you're in the REPL, enjoy LISP!
+# to open emacs, always do it from the root;
+# sudo -i
+# emacs
