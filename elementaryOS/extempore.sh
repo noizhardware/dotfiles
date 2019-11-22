aa7d60e23868eae27ca14ae34cc1f37ab69be833
noizhardware <noisefuel@gmail.com>
2019-11-22 17:54:22 +0100

    Add files via upload

---
+++ elementaryOS/extempore.sh
@@ -0,0 +1,9 @@
+# download from here and unpack: https://github.com/digego/extempore/releases
+# spin the extempore server (you need to be in the right folder of course)
+./extempore
+
+# install atom package "extempore-atom" from inside Atom
+# https://github.com/noiach/extempore-atom
+
+# use Alt+O to open hte connection between Atom and the server
+# use Alt+S to eval!
---
+++ elementaryOS/pd-l2ork_install.sh
@@ -0,0 +1,5 @@
+# sudo apt install gdebi-core -y
+
+# download pd-l2ork .deb package, go in the download folder
+sudo dpkg -i pd-l2ork-2.9.0-20190416-rev.2b3f27c-x86_64.deb
+
