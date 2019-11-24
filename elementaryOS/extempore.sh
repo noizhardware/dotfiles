a6f5d8be2f2b965c52706ac4a6045ada34f82f16
noizhardware <noisefuel@gmail.com>
2019-11-25 00:42:54 +0100

    Update extempore.sh

--- elementaryOS/extempore.sh
+++ elementaryOS/extempore.sh
@@ -1,9 +1,16 @@
 # download from here and unpack: https://github.com/digego/extempore/releases
+
+# to make this script runnable: sudo chmod +x extempore.sh (gives it the privilege to execute)
+# and then: sudo ./extempore.sh
+
 # spin the extempore server (you need to be in the right folder of course)
-./extempore
+cd /home/nff/Downloads/Extempore-0.7.0-Ubuntu/extempore/
+sudo ./extempore
+#./extempore
 
 # install atom package "extempore-atom" from inside Atom
 # https://github.com/noiach/extempore-atom
 
 # use Alt+O to open hte connection between Atom and the server
 # use Alt+S to eval!
+# use Alt+X to close the connection with the extempore server
