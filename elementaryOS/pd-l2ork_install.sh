1e9f3860eded842cb1b5ace891d76eaa4133af3b
nff-blak <noisefuel@gmail.com>
2019-11-25 15:35:05 +0100

    Θ

--- elementaryOS/pd-l2ork_install.sh
+++ elementaryOS/pd-l2ork_install.sh
@@ -3,3 +3,5 @@
 # download pd-l2ork .deb package, go in the download folder
 sudo dpkg -i pd-l2ork-2.9.0-20190416-rev.2b3f27c-x86_64.deb
 
+## poi in teoria se ci sono dependencies mancanti, dovresti fare:
+## sudo apt-get -f install
--- elementaryOS/scheme.sh
+++
@@ -1,2 +0,0 @@
-sudo apt install plt-scheme
-mzscheme
