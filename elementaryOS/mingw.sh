086c4cd3b8e3b3ab15460b06d7479c33eaddbd35
noizhardware <noisefuel@gmail.com>
2019-12-17 11:39:29 +0100

    Φ

--- elementaryOS/mingw.sh
+++ elementaryOS/mingw.sh
@@ -2,5 +2,10 @@
 sudo apt-get install mingw-w64
 sudo apt-get update
 
-# For 64-bit use: x86_64-w64-mingw32-gc++
+# For 64-bit use: x86_64-w64-mingw32-g++ << tested WORKING!!!!
 # For 32-bit use: i686-w64-mingw32-g++
+
+
+# Install a cross compiler, like mingw64 from your package manager. Then compile in the following way: instead of simply calling gcc call i686-w64-mingw32-gcc for 32-bit Windows or x86_64-w64-mingw32-gcc" for 64-bit Windows. I would also use the --static option, as the target system may not have all the libraries.
+# 
+# If you want to compile other language, like Fortran, replace -gcc with -gfortran in the previous commands.
