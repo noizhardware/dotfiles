bd14114cc6518eb2a5122cf0e829cd3a40449105
noizhardware <noisefuel@gmail.com>
2019-12-17 11:00:14 +0100

    Φ

--- elementaryOS/mingw.sh
+++ elementaryOS/mingw.sh
@@ -1,5 +1,6 @@
 # https://stackoverflow.com/questions/2033997/how-to-compile-for-windows-on-linux-with-gcc-g
 sudo apt-get install mingw-w64
+sudo apt-get update
 
 # For 64-bit use: x86_64-w64-mingw32-gc++
 # For 32-bit use: i686-w64-mingw32-g++
