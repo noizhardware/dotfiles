7d47b4db2caba82da3e1787b5b41456f12cee925
nff-bk <noisefuel@gmail.com>
2020-02-06 13:08:29 +0100

    lu

--- lubuntu/basics.sh
+++ lubuntu/basics.sh
@@ -1,5 +1,6 @@
 #dpkg --list ## see all installed packages
 apt update &&
 apt --fix-broken install &&
-apt install midori &&
+#apt install midori &&
+apt install chromium-browser &&
 apt remove firefox
