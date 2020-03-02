1dea51fc3f136ec6b7c53f0f589a3cde83811d33
nff-bk <noisefuel@gmail.com>
2020-03-02 01:52:10 +0100

    lu

--- lubuntu/basics.sh
+++ lubuntu/basics.sh
@@ -3,5 +3,6 @@ apt update &&
 apt --fix-broken install &&
 #apt install midori &&
 apt install chromium-browser &&
-apt remove firefox
+apt remove firefox &&
 ##apt install net-tools
+apt install curl
