21adcdbd43c52ebdec279b5a273541cc5000b5aa
noizhardware <noisefuel@gmail.com>
2020-03-06 20:44:44 +0100

    Φ

--- util/cpfileSSH.cmd
+++ util/cpfileSSH.cmd
@@ -3,7 +3,7 @@ SET target_ip=""
 SET target_dir="/home/user/dirname"
 SET local_file_to_transfer="R:/dir/dir/file.extension"
 
-scp %local_file_to_transfer% %target_user"@%target_ip%:%target_dir%
+scp %local_file_to_transfer% %target_user%@%target_ip%:%target_dir%
 
 REM ,,ssh
 REM ,,filetransfer
