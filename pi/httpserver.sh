86105fedd0fcd96d0a968dc2d3ae9ee4d23e27f4
noizhardware <noisefuel@gmail.com>
2020-03-07 10:04:36 +0100

    Φ

--- pi/httpserver.sh
+++ pi/httpserver.sh
@@ -2,3 +2,6 @@ sudo npm install -g pm2 &&
 sudo apt install ufw &&
 sudo ufw enable &&
 sudo ufw allow 3000
+
+
+#https://dev.to/bogdaaamn/run-your-nodejs-application-on-a-headless-raspberry-pi-4jnn
