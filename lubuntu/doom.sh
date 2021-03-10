6441987a9b533192f0f895ec33b716f5eac3752d
noizhardware <noisefuel@gmail.com>
2021-03-10 07:58:30 +0100

    nj

---
+++ lubuntu/doom.sh
@@ -0,0 +1,2 @@
+sudo apt-get install chocolate-doom doom-wad-shareware prboom-plus freedoom
+# https://securitronlinux.com/installing-and-playing-the-classic-pc-doom-game-on-linuxubuntu/
---
+++ util/apt.md
@@ -0,0 +1,1 @@
+sudo apt autoremove : to remove unused deps
