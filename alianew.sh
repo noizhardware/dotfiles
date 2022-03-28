cb789fe92d1519df53206500c2b54f6890d05cd4
noizhardware <noisefuel@gmail.com>
2022-03-28 23:00:24 +0200

    nb

---
+++ alianew.sh
@@ -0,0 +1,13 @@
+# usage: put 'source $HOME/path/to/this/alias.sh' in $HOME/.bashrc
+
+alias lsa='ls -a'
+alias lsd='ls -d'
+
+alias todo='cat $HOME/s/TODO.txt'
+alias tode='nano $HOME/s/TODO.txt'
+alias toad='$HOME/s/toad.sh'
+alias lite='$HOME/s/lite/lite ./'
+
+alias gpu='git pull origin master'
+alias gpa='git add --all && git commit -m "nb" && git push origin master'
+
