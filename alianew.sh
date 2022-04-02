ef2cf4c339a85cd4dd1e858f0f62824dbfa1e298
noizhardware <noisefuel@gmail.com>
2022-04-03 00:39:22 +0200

    nb

--- alianew.sh
+++ alianew.sh
@@ -8,10 +8,10 @@ alias tode='nano $HOME/s/TODO.txt'
 alias toad='$HOME/s/toad.sh'
 alias lite='$HOME/s/lite/lite ./'
 
-alias gstat='git status'
+alias gsta='git status'
 alias glog='git log'
-alias gpull='git pull origin master'
-alias gpush='git add --all && git commit -m "nb" && git push origin master'
+alias gpul='git pull origin master'
+alias gpus='git add --all && git commit -m "nb" && git push origin master'
 
 alias siz='du -sbh *'
 
