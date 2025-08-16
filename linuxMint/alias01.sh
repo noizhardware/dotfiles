8792d9815dbc87cdc0c53832e5679d4a355adddc
noizhardware <noizhardware@gmail.com>
2025-08-16 13:15:43 +0200

    nm

--- linuxMint/alias01.sh
+++ linuxMint/alias01.sh
@@ -1,5 +1,6 @@
 # usage: put `source /media/nff/RED/Work/git/dotfiles/linuxMint/alias01.sh` in `~/.bashrc` (and `~/.profile`) ; use `cd $HOME to go there`
-echo alias01
+echo alias01 - the Ghostless Shell
+echo 2025h16-1310
 
 alias lsa='ls -a'
 alias lsd='ls -d */'
@@ -16,11 +17,14 @@ alias glog='git log'
 alias gpul='git pull origin master'
 alias gpus='git add --all && git commit -m "nm" && git push origin master'
 
-# POWER
+#SYSTEM
+alias sys='inxi -Fxxxrzc0 --usb' # show all system info
+## POWER
 alias off='poweroff'
 alias sus='systemctl suspend'
-alias sleep='systemctl suspend'
+alias slp='systemctl suspend'
 alias hib='systemctl hibernate'
+alias rst='systemctl reboot'
 
 # META
 alias refresh='source ~/.bashrc'
@@ -33,20 +37,26 @@ alias temp='sensors'
 alias qq='exit'
 
 # PROGRAMS quick launch
-alias kan='sudo /media/nff/RED/Programzz/Linux/util/kanata --cfg /media/nff/RED/Work/git/dotfiles/kanata/kanata00.kbd'
+alias kan='sudo ~/apps/kanata --cfg /media/nff/RED/Work/git/dotfiles/kanata/kanata00.kbd'
 alias ka='/media/nff/RED/s/c/kalk/kalk'
 alias kri='~/apps/krita-5.2.11-x86_64.AppImage'
 
 # PATHs quick CD
 alias src='cd /media/nff/RED/s/c/'
 alias log='cd /media/nff/HOTdata/LOGS'
+alias dot='cd /media/nff/RED/Work/git/dotfiles'
 alias root='cd /'
 
-# night lighr
+# night light
 alias lic='gsettings set org.cinnamon.settings-daemon.plugins.color night-light-enabled false'
 alias liw='gsettings set org.cinnamon.settings-daemon.plugins.color night-light-enabled true'
 
 
-# open folders in file manager window
+# opening folders in file manager window
 alias dld='xdg-open ~/Downloads'
 alias uu='xdg-open /media/nff/RED/uu'
+
+# opening files
+alias oge='xdg-open /media/nff/HOTdata/LOGS/GELD.ods'
+alias ost='xdg-open /media/nff/RED/Work/minijack/mmix/BOMs/_stock.ods'
+alias wlo='xdg-open /media/nff/RED/MUCKE/focus/whitenoiseLow.mp3'
