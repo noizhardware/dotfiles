41f5d96608744cea1955ce7d5b534438833a14ea
noizhardware <noizhardware@gmail.com>
2025-11-11 15:54:14 +0100

    nm

--- linux/arduino.md
+++ linux/arduino.md
@@ -1,3 +1,21 @@
+2025k04-0945 ; https://docs.arduino.cc/arduino-cli/getting-started/?_gl=1*3n4gqm*_up*MQ..*_ga*ODkyMTM0MzY5LjE3NjIyNDQ4MTU.*_ga_NEXN8H46L5*czE3NjIyNDQ4MTMkbzEkZzEkdDE3NjIyNDU1NTgkajYwJGwwJGg5OTA0MTAwNzM.
+- `curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | BINDIR=/home/nff sh`
+- `export PATH="$PATH:/home/nff"`
+- `arduino-cli core update-index`
+- see connected arduino boards, and the ports they're connected to `arduino-cli board list`
+	- see the column "core" (for Arduino Micro it's `arduino:avr`)
+- install the core you need `arduino-cli core install arduino:avr`
+- see all installed cores `arduino-cli core list`
+- see all installed boards `arduino-cli board listall`
+- install a library; `arduino-cli lib install Keyboard`
+
+- compile a sketch; `arduino-cli compile --fqbn arduino:avr:micro /media/nff/RED/s/avr/nyx_wryte/nyx_wryte.ino --verbose`
+- upload a sketch; `arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:micro /media/nff/RED/s/avr/nyx_wryte/nyx_wryte.ino --verbose`
+
+
+
+---
+
 install Arduino IDE on linux;
 sudo apt update
 sudo apt install arduino
--- linuxMint/alias01.sh
+++ linuxMint/alias01.sh
@@ -1,33 +1,41 @@
 # usage: put `source /media/nff/RED/Work/git/dotfiles/linuxMint/alias01.sh` in `~/.bashrc` (and `~/.profile`) ; use `cd $HOME to go there`
 echo the Ghostless Shell
 echo $BASH_SOURCE
-echo 2025j25-1323
+echo 2025k05-2210
 
-# file system
+## file system
 	alias lsa='ls -a'
 	alias lsd='ls -d */'
 	#alias disk='df -h | grep Size && df -h | grep sda1'
 	alias disk='df -h'
 	alias siz='du -sbh *'
+	alias sizd='du -sh' # size Directory
 	alias here='pwd'
 	killdir(){
 		rm -r "$1"
 	}
 
-# GIT
+## PATH
+	export PATH="$PATH:/home/nff"
+	#export PYTHONHOME="/usr/bin"
+	#unset PYTHONHOME
+
+## GIT
 	alias gsta='git status'
 	alias glog='git log'
 	alias gpul='git pull origin master'
 	alias gpus='git add --all && git commit -m "nm" && git push origin master'
 
-#SYSTEM
+## SYSTEM
 	alias version='lsb_release -a'
 	alias term='echo $TERM'
 	alias sys='inxi -Fxxxrzc0 --usb ; mokutil --sb-state ' # show all system info
 	alias temp='sensors'
+		# sudo apt install lm-sensors
 	alias pin='ping example.com'
 	#alias speed='speedtest-cli | grep -e Download -e Upload -e ms'
-	alias speed='curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python3'
+	#alias speed='curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python3'
+	alias speed='python3 /media/nff/RED/Work/git/dotfiles/py/speedtest.py'
 	alias usbs='/media/nff/RED/Work/git/dotfiles/linuxMint/usbserial.sh'
 
 ## POWER
@@ -37,15 +45,16 @@ echo 2025j25-1323
 	alias hib='sudo systemctl hibernate'
 	alias rst='systemctl reboot'
 
-# META
+## META
 	alias refresh='source ~/.bashrc'
 	alias hh='cat /media/nff/RED/Work/git/dotfiles/linuxMint/alias01.sh'
 	alias eda='nano /media/nff/RED/Work/git/dotfiles/linuxMint/alias01.sh'
 	alias qq='exit'
-		# sudo apt install lm-sensors
+	alias cls='clear'
 
 
-# PROGRAMS quick launch
+## PROGRAMS quick launch
+	alias nano="cp /media/nff/RED/Work/git/dotfiles/nano/.nanorc $HOME/.nanorc;nano"
 	alias kan='sudo ~/apps/kanata --cfg /media/nff/RED/Work/git/dotfiles/kanata/kanata00.kbd'
 	alias ka='/media/nff/RED/s/c/kalk/kalk'
 	alias kri='~/apps/krita-5.2.11-x86_64.AppImage'
@@ -56,10 +65,12 @@ echo 2025j25-1323
 	alias bonzo='/media/nff/HOTdata/shaderz/Bonzomatic/bonzomatic /media/nff/HOTdata/shaderz/Bonzomatic/config.json'
 	alias bf='(cd /opt/betaflight/betaflight-configurator && exec sudo ./betaflight-configurator)'
 	alias eth='python3 /media/nff/RED/s/py/eth/eth.py'
-	alias zk='/media/nff/RED/s/c/zk0/zk0 /media/nff/RED/s/c/zk0/test'
+	alias zk='/media/nff/RED/s/c/zk0/zk0 /media/nff/HOTdata/LOGS/zet'
+		alias zkk='cd /media/nff/HOTdata/LOGS/zet'
+		# alias tt='nano template.md'
+		alias fle='nano /media/nff/HOTdata/LOGS/zet/fleeting.txt'
 
-
-# PATHs quick CD
+## PATHs quick CD
 	alias src='cd /media/nff/RED/s/c/'
 	alias log='cd /media/nff/HOTdata/LOGS'
 	alias dot='cd /media/nff/RED/Work/git/dotfiles'
@@ -67,20 +78,21 @@ echo 2025j25-1323
 	alias root='cd /'
 
 
-# night light
+## night light
 	alias lic='gsettings set org.cinnamon.settings-daemon.plugins.color night-light-enabled false'
 	alias liw='gsettings set org.cinnamon.settings-daemon.plugins.color night-light-enabled true'
 
 
-# opening folders in file manager window
+## opening folders in file manager window
 	alias dld='xdg-open ~/Downloads'
 	alias uu='xdg-open /media/nff/RED/uu'
 	alias muk='xdg-open /media/nff/RED/MUCKE'
 	alias dat='xdg-open /media/nff/RED/Work/electronics/datasheet/_components'
 	alias apn='xdg-open /media/nff/RED/Work/git/electronics-app-notes'
+	alias scr='xdg-open /media/nff/RED/screens'
 
-
-# opening files
+## opening files
+	alias opn='xdg-open'
 	alias oge='xdg-open /media/nff/HOTdata/LOGS/GELD.ods'
 	alias oin='xdg-open /media/nff/RED/!documents/contabilita/acc_2025/incoming.ods'
 	alias oacc='xdg-open /media/nff/RED/!documents/contabilita/acc_2025/income2025.ods'
