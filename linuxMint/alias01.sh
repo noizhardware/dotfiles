3295d5b73d45c5ee1782819406cb85dd1c118f9c
noizhardware <noizhardware@gmail.com>
2026-01-25 14:14:16 +0100

    nm

--- linuxMint/alias01.sh
+++ linuxMint/alias01.sh
@@ -1,7 +1,7 @@
 # usage: put `source /media/nff/RED/Work/git/dotfiles/linuxMint/alias01.sh` in `~/.bashrc` (and `~/.profile`) ; use `cd $HOME to go there`
 echo the Ghostless Shell
 echo $BASH_SOURCE
-echo 2026a01-2045
+echo 2026a15-1033
 
 #dircolors  --print-ls-colors
 LS_COLORS='rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:mi=00:su=37;41:sg=30;43:ca=00:tw=30;42:ow=30;43:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arc=01;31:*.arj=01;31:*.taz=01;31:*.lha=01;31:*.lz4=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.tzo=01;31:*.t7z=01;31:*.zip=01;31:*.z=01;31:*.dz=01;31:*.gz=01;31:*.lrz=01;31:*.lz=01;31:*.lzo=01;31:*.xz=01;31:*.zst=01;31:*.tzst=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.alz=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.cab=01;31:*.wim=01;31:*.swm=01;31:*.dwm=01;31:*.esd=01;31:*.avif=01;35:*.jpg=01;35:*.jpeg=01;35:*.mjpg=01;35:*.mjpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.webp=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.m4a=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.oga=00;36:*.opus=00;36:*.spx=00;36:*.xspf=00;36:*~=00;90:*#=00;90:*.bak=00;90:*.crdownload=00;90:*.dpkg-dist=00;90:*.dpkg-new=00;90:*.dpkg-old=00;90:*.dpkg-tmp=00;90:*.old=00;90:*.orig=00;90:*.part=00;90:*.rej=00;90:*.rpmnew=00;90:*.rpmorig=00;90:*.rpmsave=00;90:*.swp=00;90:*.tmp=00;90:*.ucf-dist=00;90:*.ucf-new=00;90:*.ucf-old=00;90:';
@@ -44,6 +44,7 @@ export LS_COLORS
 	alias temp='sensors'
 	# alias temp='xsensors'
 		# sudo apt install lm-sensors
+	alias gpu='nvidia-smi'
 	alias pin='ping example.com'
 	#alias speed='speedtest-cli | grep -e Download -e Upload -e ms'
 	#alias speed='curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python3'
@@ -64,6 +65,7 @@ export LS_COLORS
 
 ## META
 	alias rld='source ~/.bashrc'
+	alias rrr='rld'
 	alias hh='cat /media/nff/RED/Work/git/dotfiles/linuxMint/alias01.sh'
 	alias eda='nano /media/nff/RED/Work/git/dotfiles/linuxMint/alias01.sh'
 	alias qq='exit'
@@ -103,9 +105,10 @@ export LS_COLORS
 ## PATHs quick CD
 	alias src='cd /media/nff/RED/s/c/'
 	alias log='cd /media/nff/HOTdata/LOGS'
-	alias dot='cd /media/nff/RED/Work/git/dotfiles'
+	alias dof='cd /media/nff/RED/Work/git/dotfiles'
 	alias site='cd /media/nff/RED/Work/git/noizhardware.github.io'
 	alias root='cd /'
+	alias gvi='cd /media/nff/HOTdata/graphViz'
 
 
 ## night light
@@ -122,12 +125,13 @@ export LS_COLORS
 	alias apn='xdg-open /media/nff/RED/Work/git/electronics-app-notes'
 	alias scr='xdg-open /media/nff/RED/screens'
 	alias rec='xdg-open mtp://Google_Pixel_2_HT79P1A02452/Internal%20shared%20storage/SmartMob/SmartRecorder'
+	alias arc='xdg-open /media/nff/C2E0E61BE0E61587/Users/nff/Videos/NVIDIA/Arc\ Raiders'
 
 ## opening files
 	alias opn='xdg-open'
 	alias oge='(xdg-open /media/nff/HOTdata/LOGS/GELD.ods &)'
 	alias oin='xdg-open /media/nff/RED/!documents/contabilita/acc_2025/incoming.ods'
-	alias oacc='xdg-open /media/nff/RED/!documents/contabilita/acc_2025/income2025.ods'
+	alias oacc='xdg-open /media/nff/RED/!documents/contabilita/acc_2026/income2026.ods'
 	alias ofor='xdg-open /media/nff/RED/!documents/contabilita/forfettaria2026.ods'
 	alias ost='xdg-open /media/nff/RED/Work/minijack/mmix/BOMs/_stock.ods'
 	alias oml='xdg-open /media/nff/RED/Work/minijack/mailinglist.ods'
