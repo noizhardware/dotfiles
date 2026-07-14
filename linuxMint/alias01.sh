290e19b13c779e3cbe1a857c66b536d9ac2c5f64
noizhardware <noizhardware@gmail.com>
2026-07-14 12:06:43 +0200

    nm

--- linuxMint/alias01.sh
+++ linuxMint/alias01.sh
@@ -1,7 +1,7 @@
 # usage: put `source /media/nff/RED/Work/git/dotfiles/linuxMint/alias01.sh` in `~/.bashrc` (and `~/.profile`) ; use `cd $HOME to go there`
 echo the Ghostless Shell
 echo $BASH_SOURCE
-echo 2026f18-1300
+echo 2026g10-1259
 
 #dircolors  --print-ls-colors
 LS_COLORS='rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:mi=00:su=37;41:sg=30;43:ca=00:tw=30;42:ow=30;43:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arc=01;31:*.arj=01;31:*.taz=01;31:*.lha=01;31:*.lz4=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.tzo=01;31:*.t7z=01;31:*.zip=01;31:*.z=01;31:*.dz=01;31:*.gz=01;31:*.lrz=01;31:*.lz=01;31:*.lzo=01;31:*.xz=01;31:*.zst=01;31:*.tzst=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.alz=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.cab=01;31:*.wim=01;31:*.swm=01;31:*.dwm=01;31:*.esd=01;31:*.avif=01;35:*.jpg=01;35:*.jpeg=01;35:*.mjpg=01;35:*.mjpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.webp=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.m4a=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.oga=00;36:*.opus=00;36:*.spx=00;36:*.xspf=00;36:*~=00;90:*#=00;90:*.bak=00;90:*.crdownload=00;90:*.dpkg-dist=00;90:*.dpkg-new=00;90:*.dpkg-old=00;90:*.dpkg-tmp=00;90:*.old=00;90:*.orig=00;90:*.part=00;90:*.rej=00;90:*.rpmnew=00;90:*.rpmorig=00;90:*.rpmsave=00;90:*.swp=00;90:*.tmp=00;90:*.ucf-dist=00;90:*.ucf-new=00;90:*.ucf-old=00;90:';
@@ -49,7 +49,7 @@ export LS_COLORS
 	# alias temp='xsensors'
 		# sudo apt install lm-sensors
 	alias tem='temp 2>/dev/null | grep "Package id 0"'
-	alias gpu='nvidia-smi'
+	alias gpu='nvidia-smi ; nvidia-smi | grep -i p8'
 	alias pin='ping example.com'
 	#alias speed='speedtest-cli | grep -e Download -e Upload -e ms'
 	#alias speed='curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python3'
@@ -83,6 +83,8 @@ export LS_COLORS
 	alias nano="cp /media/nff/RED/Work/git/dotfiles/nano/.nanorc $HOME/.nanorc;nano"
 	alias kan='sudo ~/apps/kanata --cfg /media/nff/RED/Work/git/dotfiles/kanata/kanata00.kbd'
 	alias kal='/media/nff/RED/s/c/kalk/kalk'
+	alias noo='/media/nff/RED/s/c/noote/noote'
+	alias goo='katrain'
 	alias kri='~/apps/krita-5.2.11-x86_64.AppImage'
 	alias mass='/media/nff/RED/s/c/mass/mass'
 	alias alk='/media/nff/RED/s/c/alk/alk /media/nff/HOTdata/LOGS/alc.MASS'
@@ -102,7 +104,8 @@ export LS_COLORS
 		
 		# /home/nff/Downloads/yt-dlp_linux/yt-dlp_linux "$1" -t mp3 -P "~/Downloads" --yes-playlist -o "%(playlist_index&{} - |)s%(title)s.%(ext)s"
 		# /home/nff/Downloads/yt-dlp_linux/yt-dlp_linux "$1" --cookies-from-browser vivaldi -t mp3 -P "~/Downloads" --yes-playlist -o "%(playlist_index&{} - |)s%(title)s.%(ext)s"
-		/home/nff/Downloads/yt-dlp_linux/yt-dlp_linux "$1" -t mp3 -P "~/Downloads" --yes-playlist -o "%(playlist_index&{} - |)s%(title)s.%(ext)s" --cookies-from-browser vivaldi:/home/nff/.config/vivaldi
+		# /home/nff/Downloads/yt-dlp_linux/yt-dlp_linux "$1" -t mp3 -P "~/Downloads" --yes-playlist -o "%(playlist_index&{} - |)s%(title)s.%(ext)s" --cookies-from-browser vivaldi:/home/nff/.config/vivaldi
+		/home/nff/Downloads/yt-dlp_linux/yt-dlp_linux "$1" -t mp3 -P "~/Downloads" --yes-playlist -o "%(playlist_index&{} - |)s%(title)s.%(ext)s"
 	}
 	# sea(){ #search ; usage; sea [filematch][searchterm] ; eg; `sea "*.md" porcodio`
 	# 	find . -type f -name "$1" -print0 | xargs -0 grep --color -H -i "$2"
@@ -157,11 +160,13 @@ export LS_COLORS
 	alias tim='/media/nff/RED/Work/git/dotfiles/util/tai-clock.sh'
 
 ## ZK0
-	alias zk='/media/nff/RED/s/c/zk0/bin/zk0 /media/nff/HOTdata/LOGS/zet'
-		alias zkk='cd /media/nff/HOTdata/LOGS/zet'
-		# alias tt='nano template.md'
-		alias fle='nano /media/nff/HOTdata/LOGS/zet/fleeting.txt'
-		alias nn='/media/nff/HOTdata/LOGS/zet/new.sh'
+	ZK0BIN=/media/nff/RED/s/c/zk0/bin/zk0
+	ZETPATH=/media/nff/HOTdata/LOGS/zet
+	alias zk='$ZK0BIN $ZETPATH'
+	alias zkk='cd $ZETPATH'
+	# alias tt='nano template.md'
+	alias fle='nano $ZETPATH/fleeting.txt'
+	alias nn='$ZETPATH/new.sh'
 
 ## PATHs quick CD
 	alias src='cd /media/nff/RED/s/c/'
---
+++ util/copyDirectory.md
@@ -0,0 +1,10 @@
+`sudo cp -r /path/to/source /path/to/dest`
+copies the source folder into dest, creating `dest/source`
+
+`sudo cp -r /path/to/source/. /path/to/dest`
+copies the contents of source into dest directly
+
+keywords;
+copy directory folder terminal linux
+
+created: 2026g06-2217
